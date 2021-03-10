d56ae84205af0e021eb3906f6fbf34337a52ba52
Rafael Nikolaou <rnicolaouceng@gmail.com>
2021-03-10 17:47:19 +0200

    2 New codewars

--- week-ending-13-03/main.js
+++ week-ending-13-03/main.js
@@ -75,3 +75,27 @@ class SmallestIntegerFinder {
   }
 }
  
+//kyu 7:
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+//code:
+function disemvowel(str) {
+  str = str.replace(/[aAeEuUoOiI]/g,'')
+  return str;
+}
+
+
+//kyu 7:
+//You are going to be given a word. Your job is to return the middle character of the word.
+//  If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+
+//code:
+function getMiddle(s)
+{
+  if(s.length % 2 === 0){
+    return s.charAt((s.length/2-1))+''+s.charAt((s.length/2))
+  }
+  else
+    return s.charAt(Math.ceil(s.length/2-1))
+}
