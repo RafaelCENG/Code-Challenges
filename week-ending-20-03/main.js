6e7f3c9ff494397218addab869051556a829ea2a
Rafael Nikolaou <rnicolaouceng@gmail.com>
2021-03-17 22:57:30 +0200

    4 more Codewars

--- week-ending-20-03/main.js
+++ week-ending-20-03/main.js
@@ -41,3 +41,69 @@ function greet(){
 function century(year) {
   return Math.ceil(year/100)
 }
+
+//kyu: 7
+//Given an integral number, determine if it's a square number:
+
+//code
+var isSquare = function(n){
+  let rooted = Math.sqrt(n).toFixed(0)
+  if(rooted * rooted === n){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+//kyu: 8
+//Instructions:The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+//code
+function basicOp(operation, value1, value2)
+{
+ if (operation === '+'){
+   return value1 + value2
+ }
+  else if(operation === '-'){
+    return value1 - value2
+  }
+  else if(operation === '*'){
+    return value1*value2
+  }
+  else if(operation === '/')
+    return value1/value2
+}
+
+//kyu: 8
+//Instructions: Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+//code
+function isDivisible(n, x, y) {
+  if(n % x === 0 && n % y === 0){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+//kyu: 8
+//Instructions:We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+//code
+var stringToNumber = function(str){
+  return Number(str)
+}
+
+//kyu: 7
+//Instructions: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+//code
+function highAndLow(numbers){
+  let num = numbers.split(" ").map(Number)
+  let high = Math.max(...num)
+  let lowest = Math.min(...num)
+  return `${high} ${lowest}`
+}
