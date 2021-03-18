c4a16494907f9bbb5a5a0c6fb4e2f79c130efdd3
Rafael Nikolaou <rnicolaouceng@gmail.com>
2021-03-18 16:22:11 +0200

    Added Codewars

--- week-ending-20-03/main.js
+++ week-ending-20-03/main.js
@@ -107,3 +107,70 @@ function highAndLow(numbers){
   let lowest = Math.min(...num)
   return `${high} ${lowest}`
 }
+
+//kyu: 8
+//Instructions: Write a function findNeedle() that takes an array full of junk but containing one "needle"
+                // After your function finds the needle it should return a message (as a string) that says:
+                // "found the needle at position " plus the index it found the needle, so: 
+
+//code
+function findNeedle(haystack) {
+  if (haystack.includes('needle') === true){
+    return 'found the needle at position ' + haystack.indexOf("needle")
+  }
+}
+
+//kyu: 8
+//Instructions: Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+
+//code
+function maps(x){
+  return x.map(i => i*2)
+}
+
+//kyu: 8
+//Instructions: Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// Patrick Feeney => P.F
+
+//code
+function abbrevName(name){
+  const words = name.split(' ')
+  let firstCapital = words[0].charAt(0)
+  let secondCapital = words[1].charAt(0)
+  return firstCapital.toUpperCase()+'.'+secondCapital.toUpperCase()
+}
+
+//kyu: 7
+//Instructions: Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+              //Essentially, rearrange the digits to create the highest possible number.
+
+//code
+function descendingOrder(n){
+  let numArr = Array.from(String(n),Number)
+  numArr = numArr.sort((a,b) => b-a )
+  return Number(numArr.join(''))
+}
+
+//kyu: 7
+//Instructions: This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+//code
+function accum(s) {
+	let str = s.split('')
+  str.forEach((x,i) => {
+    str[i] = x.toUpperCase()+x.repeat(i).toLowerCase()
+  })
+  return  str.join('-').toString()
+}
