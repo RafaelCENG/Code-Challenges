2684de1b717bc5ceaeed1bcffc46414bbd73c686
Rafael Nikolaou <rnicolaouceng@gmail.com>
2021-03-21 17:33:33 +0200

    Added 1 codewar

--- week-ending-27-03/main.js
+++ week-ending-27-03/main.js
@@ -135,3 +135,20 @@ function arrayPlusArray(arr1, arr2) {
 function bonusTime(salary, bonus) {
   return  (bonus ? "\u00A3"+salary*10 : "\u00A3"+salary)
 }
+
+//kyu: 8
+//title : Fake Binary
+
+//code
+function fakeBin(x){
+  let arr = x.split('')
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] < 5){
+      arr[i] = 0
+    }
+    else{
+      arr[i] = 1
+    }
+  }
+  return arr.join('')
+}
