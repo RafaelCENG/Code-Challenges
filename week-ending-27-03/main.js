0d4dd3b9ea95c56cb8298427d9a3c8c4b83deffd
Rafael Nikolaou <rnicolaouceng@gmail.com>
2021-03-27 12:16:57 +0200

    Added codewars

--- week-ending-27-03/main.js
+++ week-ending-27-03/main.js
@@ -363,3 +363,55 @@ function rentalCarCost(d) {
     return d * 40
   }
 }
+
+
+//kyu: 8
+//title : How good are you really?
+
+//code
+function betterThanAverage(classPoints, yourPoints) {
+  let classSum = classPoints.reduce((acc,curr) => acc + curr)
+  let classAvg = classSum / classPoints.length
+  return yourPoints > classAvg ? true : false
+}
+
+//kyu: 8
+//title : Will you make it?
+
+//code
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return mpg*fuelLeft >= distanceToPump ? true : false
+};
+
+
+//kyu: 8
+//title : Calculate BMI
+
+//code
+function bmi(weight, height) {
+  let bmi = weight / (height*height)
+  if(bmi <= 18.5){
+    return 'Underweight'
+  }
+  else if(bmi <=25.0){
+    return 'Normal'
+  }
+  else if(bmi <=30.0){
+    return 'Overweight'
+  }
+  else{
+    return 'Obese'
+  }
+}
+
+//kyu: 8
+//title : Count by X
+
+//code
+function countBy(x, n) {
+  var z = [];
+  for (let i = 1; i <= n; i++){
+    z.push(i*x)
+  }
+  return z;
+}
