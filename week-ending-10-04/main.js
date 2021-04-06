aed579bd1e7293246104699527f148de050fe43f
Rafael Nikolaou <rnicolaouceng@gmail.com>
2021-04-06 23:07:26 +0300

    New Codewars

--- week-ending-10-04/main.js
+++ week-ending-10-04/main.js
@@ -11,3 +11,23 @@ function divisibleBy(numbers, divisor){
     }
     return arr
   }
+
+// kyu 8:
+// Title: Unfinished Loop - Bug Fixing #1
+
+//code:
+function createArray(number){
+  var newArray = [];
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  return newArray;
+}
+
+// kyu 8:
+// Title: Remove exclamation marks
+
+//code:
+function removeExclamationMarks(s) {
+  return s.replace(/!/g,"")
+}
