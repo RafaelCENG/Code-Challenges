69876c7f663ffb68edf447b04d9ced2c1cc8aaa9
unknown <rnicolaouceng@gmail.com>
2021-04-18 18:42:30 +0300

    New codewar

---
+++ week-ending-24-04/main.js
@@ -0,0 +1,7 @@
+// kyu 8:
+// Title: Drink about
+
+//code:
+function peopleWithAgeDrink(old) {
+  return old >= 21 ? 'drink whisky' : old >= 18 ? 'drink beer' : old >= 14 ? 'drink coke' : 'drink toddy'
+};
